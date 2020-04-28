cca86d40c67369f0b1ceae762bad21fd6a8ca756
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2020-04-29 01:41:24 +0300

    initial commit + project structure main setup

---
+++ course-flux/src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+      <h2> About </h2>
+      <p>This app was built with React</p>
+    </>
+  );
+}
+
+export default AboutPage;
