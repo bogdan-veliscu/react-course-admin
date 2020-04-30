f7303ef1accaff0df9d601e1c3d8218bea6e2607
Bogdan Veliscu <bogdan.veliscu@outlook.com>
2020-04-30 12:15:13 +0300

    added basic navigation menu and course list comp

--- course-flux/src/index.js
+++ course-flux/src/index.js
@@ -1,8 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
 
-import React from 'react';
-import { render } from 'react-dom';
-
-import HomePage from './components/HomePage';
-
-render( < HomePage / > , document.getElementById('root'));
+render(<App />, document.getElementById("root"));
